01f939e1645e37f848a6165e02aed384859f07ca
PankajGoswami11 <123570407+PankajGoswami11@users.noreply.github.com>
2025-04-04 09:14:30 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as nwewe version
+
+// alert(3+3)//we are using nodejs, not browser
+
+console.log(3+3);
+// code readability should be high
+console.log("Pankaj");
+
+let name = "panakj";
+let age = 18;
+let isLoggedIn = false;
+
+//number => 2 to power 53;
+// bigint for big number;
+// string => "" ''
+// boolean => true/false;
+// null => standalone value // null is  emptuy value
+// undefine => variable without value or variable whithout  
+// symbol => unique
+
+// objects
+
+console.log(typeof age);
+
+console.log(typeof null);        // object
+console.log(typeof undefined);  // undefined
