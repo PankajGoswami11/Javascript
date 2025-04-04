188eefdbd0d22d150a7b60a894473f38a2342383
PankajGoswami11 <123570407+PankajGoswami11@users.noreply.github.com>
2025-04-04 09:13:59 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,27 @@
+const accountId = 144553;
+let accountEmail = "panakjgoswami1109@gmail.com";
+var accountPassword = "123345";
+accountCity = "Delhi";
+
+let accountState;
+
+// accountId = 2; //Not allowed to change const after declration
+console.log(accountId);
+accountPassword = "12345";
+accountEmail ="pankaj@gamil.com";
+accountCity = "Bengaluru";
+console.log(accountId);
+console.log(accountPassword);
+console.log(accountEmail);
+console.log(accountCity);
+
+console.table([accountId,accountEmail,accountCity,accountCity])
+
+
+/*
+ prefer not ot use var
+ because of issue in blocke scope and functional scope
+ */
+
+ 
+
